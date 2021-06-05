ff1b44dc526035a4a3822bfad824a3399367909c
metesv <metehansivri1@gmail.com>
2021-06-06 00:29:40 +0300

    Refactor usecontext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
-
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
+import axios from "axios";
+
+import "./App.css";
 
 import Homepage from "./pages/Homepage";
 import Contact from "./pages/Contact";
@@ -8,15 +10,27 @@ import ProductDetails from "./pages/ProductDetails";
 
 import Navbar from "./components/Navbar";
 
+export const ClothesContext = React.createContext();
+
 function App() {
+  const [clothes, setClothes] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://60bbc6ef3a39900017b2de9f.mockapi.io/clothes")
+      .then(response => setClothes(response.data));
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/:id" children={<ProductDetails />} />
-        <Route path="/contact" exact component={Contact} />
-      </Switch>
+      <ClothesContext.Provider value={clothes}>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/:id" children={<ProductDetails />} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </ClothesContext.Provider>
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
 import styled from "styled-components";
 
 import ProductCard from "../components/ProductCard";
+import { ClothesContext } from "../App";
 
 const StyledDiv = styled.div`
   display: block;
 `;
 
 function Homepage() {
-  const [clothes, setClothes] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("https://60bbc6ef3a39900017b2de9f.mockapi.io/clothes")
-      .then(response => setClothes(response.data));
-  }, []);
+  const clothes = useContext(ClothesContext);
 
   return (
     <StyledDiv>
